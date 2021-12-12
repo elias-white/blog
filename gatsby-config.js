01bb57102543f231437be676db15f1f8aaa4e9fc
Elias White <github@eliaswhite.com>
2021-12-12 11:18:30 +0800

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "Elias's Personal Site",
+    title: "Elias White",
   },
   plugins: [
       "gatsby-plugin-image",
-      "gatsby-plugin-sharp"
+      "gatsby-plugin-sharp",
+      {
+          resolve: "gatsby-source-filesystem",
+          options: {
+              name: `blog`,
+              path: `${__dirname}/blog`,
+          }
+      },
   ],
 };
