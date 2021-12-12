cd4cf82d7db377d1caad51a0d9b4af6fac39aa55
Elias White <github@eliaswhite.com>
2021-12-12 10:25:29 +0800

    Finish part 3: plugins of Gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
     title: "Elias's Personal Site",
   },
-  plugins: [],
+  plugins: [
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp"
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
 	return (
 		<Layout pageTitle={"Home"}>
 			<p>Welcome to my website.</p>
+			<StaticImage
+				alt="EFF Logo"
+				src="https://www.eff.org/files/2018/06/14/eff-logo-lockup-1color.png"
+			/>
 		</Layout>
 	)
 }
